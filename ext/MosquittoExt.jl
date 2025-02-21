3167cc16da3110e1bf0eed469eee8a7b9c9a27a6
Daniel Muschick <daniel.muschick@best-research.eu>
2025-02-21 15:07:45 +0100

    Adds callbacks for connection and disconnection so that synchronous calls are possible

--- ext/MosquittoExt.jl
+++ ext/MosquittoExt.jl
@@ -90,6 +90,7 @@ Structure containing all information for encapsulating the conncetion to the Mos
 
   - `connection_config::MosquittoConnectionConfig`: Configuration required to establish a connection
   - `pub_inflight::Dict{Cint, Distributed.Future}`: Dict from message ID to future somebody is waiting on when publishing synchronously
+  - `connect_callback::Union{Nothing,Distributed.Future}`: Future that should be set once the CONNACK message was received from the broker
   - `callbacks::Dict{String, OnMessage}`: Dict from topic one has subscribed to to callback that should be called with the data
   - `loop_task::Union{Nothing, Task}`: Task storing the loop that checks for new subscriptions
   - `is_running::Ref{Bool}`: Reference shared with the task to stop it when client is disconnected
@@ -98,14 +99,31 @@ mutable struct MosquittoClientConfig <: MQTT.AbstractConnection
     client::Mosquitto.Client
     connection_config::MosquittoConnectionConfig
     pub_inflight::Dict{Cint,Distributed.Future}
+    connect_callback::Union{Nothing,Distributed.Future}
+    disconnect_callback::Union{Nothing,Distributed.Future}
     callbacks::Dict{String, MQTT.OnMessage}
     loop_task::Union{Nothing,Task}
     is_running::Ref{Bool}
+    is_loop_running::Bool
+end
+function MosquittoClientConfig(client::Mosquitto.Client; kwargs...)
+    return MosquittoClientConfig(
+        client, 
+        MosquittoConnectionConfig(; kwargs...), 
+        Dict{Cint,Distributed.Future}(), # pub_inflight
+        nothing, # connect_callback
+        nothing, # disconnect_callback
+        Dict{String, MQTT.OnMessage}(), # callbacks
+        nothing, # loop_task
+        Ref{Bool}(false), # is_running
+        false, # is_loop_running
+    )
 end
 function MosquittoClientConfig(; kwargs...)
     client = Mosquitto.Client()
-    return MosquittoClientConfig(client, MosquittoConnectionConfig(; kwargs...), Dict{Cint,Distributed.Future}(), Dict{String, MQTT.OnMessage}(), nothing, Ref{Bool}(false))
+    return MosquittoClientConfig(client; kwargs...)
 end
+
 """
     MQTT.MQTTConnection(client::Mosquitto.Client; kwargs...)
 
@@ -123,14 +141,14 @@ Create an MosquittoClientConfig object (an `AbstractConnection` for MQTT using `
   - `keepalive::Int`: Time in seconds before connection to broker is disconnected. Default: 60.
 """
 function MQTT.MQTTConnection(client::Mosquitto.Client; kwargs...)
-    return MosquittoClientConfig(client, MosquittoConnectionConfig(; kwargs...), Dict{Cint,Distributed.Future}(), Dict{String, MQTT.OnMessage}(), nothing, Ref{Bool}(false))
+    return MosquittoClientConfig(client; kwargs...)
 end
 
 function MQTT._resolve(future::Distributed.Future)
     return resolve(future)
 end
 
-function MQTT._connect(c::MosquittoClientConfig)
+function MQTT._connect(c::MosquittoClientConfig) # future contains a Mosuitto.MosquittoCwrapper.mosq_err_t
     if !isempty(c.connection_config.certfile_server)
         ret = if !isempty(c.connection_config.certfile_client)
             Mosquitto.tls_set(c.client, c.connection_config.certfile_server; certfile=c.connection_config.certfile_client, keyfile=c.connection_config.keyfile_client)
@@ -139,8 +157,6 @@ function MQTT._connect(c::MosquittoClientConfig)
         end
         if ret != Mosquitto.MosquittoCwrapper.MOSQ_ERR_SUCCESS
             error("Error while trying to establish encrypted communication: $ret")
-        else
-            @info "Successfully set TLS"
         end
     end
     flag = Mosquitto.connect(
@@ -151,20 +167,20 @@ function MQTT._connect(c::MosquittoClientConfig)
     )
 
     if flag == Mosquitto.MosquittoCwrapper.MOSQ_ERR_SUCCESS
-        @info "Spawning the loop task"
+        c.is_loop_running && @error("MQTT loop is already running. Something must have gone wrong.")
         c.pub_inflight = Dict{Cint, Distributed.Future}()
         c.callbacks = Dict{String, MQTT.OnMessage}() # get rid of invalid callbacks
         c.is_running[] = true # must be set before we spawn the thread
+        c.connect_callback = Distributed.Future() # will be called in the loop if CONNACK is received
+        c.disconnect_callback = nothing
         c.loop_task = Threads.@spawn loop(c)
+        return c.connect_callback
     end
-    # TODO: Is there a method in Mosquitto to wait until the CONNACK was received?
-    future = encapsulateInFuture(flag)
 
-    # TODO: Start the loop asynchronously
-    return future
+    return encapsulateInFuture(flag)
 end
 
-function MQTT._subscribe(callback::MQTT.OnMessage, c::MosquittoClientConfig, topic::AbstractString, qos::MQTT.QOS)
+function MQTT._subscribe(callback::MQTT.OnMessage, c::MosquittoClientConfig, topic::AbstractString, qos::MQTT.QOS) # returns a tuple {Mosquitto.MosquittoCwrapper.mosq_err_t, Cint}
     # Sometimes we listen to a general topic "topic/+" --> restrict it to "topic"
     rv, message_id = Mosquitto.subscribe(c.client, String(topic); qos=mosquittoQOS(qos))
     if rv == Mosquitto.MosquittoCwrapper.MOSQ_ERR_SUCCESS
@@ -173,7 +189,7 @@ function MQTT._subscribe(callback::MQTT.OnMessage, c::MosquittoClientConfig, top
     return encapsulateInFuture((rv, message_id))
 end
 
-function MQTT._publish(c::MosquittoClientConfig, topic::AbstractString, payload, qos::MQTT.QOS, retain)
+function MQTT._publish(c::MosquittoClientConfig, topic::AbstractString, payload, qos::MQTT.QOS, retain) # returns a tuple {Mosquitto.MosquittoCwrapper.mosq_err_t, Cint}
     rv, message_id = Mosquitto.publish(c.client, String(topic), payload; qos=mosquittoQOS(qos), retain)
 
     # One can wait for the result if one is interested; will be put on the get_pub_channel(client)
@@ -196,12 +212,19 @@ end
 function MQTT._disconnect(c::MosquittoClientConfig)
     flag = Mosquitto.disconnect(c.client)
     c.is_running[] = false
-    # TODO: future does not really work well with actually stopping everything --> should contain wait(task)
+
+    if flag == Mosquitto.MosquittoCwrapper.MOSQ_ERR_SUCCESS && c.is_loop_running
+        # Return a future that will be called from the main loop as soon as it stops
+        c.disconnect_callback = Distributed.Future()
+        return c.disconnect_callback
+    end
+
     return encapsulateInFuture(flag)
 end
 
 function loop(c::MosquittoClientConfig)
-    @info "Starting Mosquitto loop"
+    @info "MQTT.MosquittoExt.loop: Starting Mosquitto loop"
+    c.is_loop_running = true
 
     while c.is_running[]
         # @info "Running mosquitto loop..."
@@ -212,11 +235,25 @@ function loop(c::MosquittoClientConfig)
         connect_channel = Mosquitto.get_connect_channel(c.client)
 
         # Check if publishing has finished --> somehow this never reacted :-(
+        while isready(connect_channel)
+            connect_cb = take!(connect_channel) # a Mosquitto.ConnectionCB object
+            @info "MQTT.MosquittoExt.loop: Received connect message with content: $connect_cb"
+            if connect_cb.val == 1 && !isnothing(c.connect_callback) # 0 on disconnect, 1 on connect
+                put!(c.connect_callback, connect_cb.returncode)
+                c.connect_callback = nothing
+            end
+            if connect_cb.val == 0 && !isnothing(c.disconnect_callback)
+                put!(c.disconnect_callback, connect_cb.returncode)
+                c.disconnect_callback = nothing
+            end
+        end
+            
+        # Check if publishing has finished --> somehow this never reacted :-(
         while isready(pub_channel)
             message_id = take!(pub_channel)
-            @info "Received message with ID $message_id"
+            # @info "Received message with ID $message_id"
             haskey(c.pub_inflight, message_id) || continue
-            @info "Putting on future for message with ID $message_id"
+            # @info "Putting on future for message with ID $message_id"
             put!(c.pub_inflight[message_id], (Mosquitto.MosquittoCwrapper.MOSQ_ERR_SUCCESS, message_id)) # same message as would have been returned directly
             delete!(c.pub_inflight, message_id)
         end
@@ -225,18 +262,24 @@ function loop(c::MosquittoClientConfig)
             message = take!(msg_channel) # a Mosquitto.MessageCB object
             # for searching, take the more general topic without the number
             topic_match = findmatch(message.topic, collect(keys(c.callbacks)))
-            @info "Received message for topic $(message.topic)"
             if isnothing(topic_match) || !haskey(c.callbacks, topic_match)
+                @info "Received message for topic $(message.topic), but found no available match"
                 continue
             end
-            @info "Calling callback for message $topic_match"
+            @info "Received message for topic $(message.topic), matching with $topic_match"
             @invokelatest c.callbacks[topic_match](message.topic, message.payload) # not sure why invokelatest is necessary??? Gives me "The applicable method may be too new: running in world age 31547, while current world is 31548" without
-            @info "Callback finished"
         end
 
-        sleep(0.5)
+        sleep(0.25)
     end
-    @info "Stopped Mosquitto loop task"
+
+    c.is_loop_running = false
+    # If the connection was lost before, the disconnect callback is not called
+    if !isnothing(c.disconnect_callback)
+        put!(c.disconnect_callback, Mosquitto.MosquittoCwrapper.MOSQ_ERR_CONN_LOST)
+        c.disconnect_callback = nothing
+    end
+    @info "MQTT.MosquittoExt.loop: Stopped Mosquitto loop"
     return nothing
 end
 
